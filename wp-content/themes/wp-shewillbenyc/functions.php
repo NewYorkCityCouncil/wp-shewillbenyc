d1ce3e7c4321b19a384e1d5d79580cbe4345491c
Andy Cochran <acochran@council.nyc.gov>
2017-04-13 12:00:53 -0400

    add function for referencing theme path in the_content

--- wp-content/themes/wp-shewillbenyc/functions.php
+++ wp-content/themes/wp-shewillbenyc/functions.php
@@ -2,3 +2,12 @@
 
 require_once(get_template_directory().'/assets/functions/enqueue-scripts.php');
 require_once(get_template_directory().'/assets/functions/menus.php');
+
+
+/**
+ * Get stylesheet directory for images
+ */
+function child_shortcode() {
+  return get_bloginfo('stylesheet_directory');
+}
+add_shortcode('theme-path', 'child_shortcode');
